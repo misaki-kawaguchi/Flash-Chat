bca30c0e16b34313654a1a7e9f3b286d9846e8c1
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-24 22:18:59 +0900

    ルート設定

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,13 @@ class FlashChat extends StatelessWidget {
         ),
       ),
       home: WelcomeScreen(),
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_Screen': (context) => ChatScreen(),
+      },
     );
   }
 }
