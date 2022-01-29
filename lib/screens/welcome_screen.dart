eadcf155e5b58e17807f7bf2e0064f87596e6fbe
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-29 21:17:54 +0900

    animated_text_kitをインポート

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
 
