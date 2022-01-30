c24f10e244588d9beb6449879e671d63aa980ae4
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-30 23:02:45 +0900

    modal_progress_hud.dartをインポート

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = "registration_screen";
