0b542e471e2a078d43513f15aee685c9bb4ca40c
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-30 20:53:30 +0900

    ユーザーが入力した内容を保存

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -10,6 +10,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +37,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration:
                 kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
@@ -43,6 +48,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration:
                 kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
