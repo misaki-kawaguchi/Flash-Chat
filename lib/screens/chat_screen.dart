2cafc7c778c5475cd5299d8cf0649444ce030155
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-30 21:37:20 +0900

    メールアドレスのログが出るようにする

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -31,6 +31,7 @@ class _ChatScreenState extends State<ChatScreen> {
       final user = await _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
+        print(loggedInUser.email);
       }
     }
     catch(e) {
